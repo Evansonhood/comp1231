52fb0b53b38fca2a4f36bb454564e2155d6423f7
Evansonhood <59575562+Evansonhood@users.noreply.github.com>
2020-01-13 16:13:42 -0500

    Add files via upload

---
+++ week-02/examples/index.js
@@ -0,0 +1,7 @@
+<script>
+var x = 5;
+var y = 6;
+var z = x + y;
+document.getElementById("index.html").innerHTML =
+"The value of z is: " + z;
+</script>
---
+++ week-02/exercises/index.js
@@ -0,0 +1,5 @@
+const var1 = 9;
+let var2 = 6;
+var var3 = var1 + var2;
+console.log('it works!');
+console.log(var1, var2, var3)
